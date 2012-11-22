ef402be30d51b192aac9d6956925eb6d6dfa8005
Jaeho Shin <netj@sparcs.org>
2012-11-22 01:17:16 -0800

    timeout hanging imagesnap in TakeOneLoginShot

--- Mac/Scripts/TakeOneLoginShot.sh
+++ Mac/Scripts/TakeOneLoginShot.sh
@@ -10,7 +10,7 @@
 # Created: 2012-11-17
 set -eu
 
-: ${DateFormat:=%Y/%m/%d-%H%M%S} ${JPEGQuality:=20}
+: ${DateFormat:=%Y/%m/%d-%H%M%S} ${NumRetries:=3} ${JPEGQuality:=20}
 
 [ -d ~/.loginshots ] || ln -sfn ~/Dropbox/Photos/LoginShots ~/.loginshots
 LoginShotsFolder=~/.loginshots
@@ -21,7 +21,16 @@ filename="$LoginShotsFolder/$(date +"$DateFormat")$event.jpg"
 mkdir -p "$(dirname "$filename")"
 
 # take a shot
-imagesnap "$filename"
+for i in $(seq $NumRetries); do
+    imagesnap "$filename" & pid=$!
+    sleep 3
+    if [ -s "$filename" ]; then
+        break
+    elif ps -o pid= -p $pid &>/dev/null; then
+        kill $pid
+    fi
+done
+[ -s "$filename" ] || echo >&2 "imagesnap failed ($NumRetries tries) for $filename"
 
 # and compress
 sips -s format jpeg -s formatOptions $JPEGQuality "$filename"
