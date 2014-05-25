17b5f9b440091821ae72b955cd4861dac2e12291
Jaeho Shin <netj@sparcs.org>
2014-05-25 05:03:32 -0700

    Fixed MacVim note creater to ask filename

--- Mac/Scripts/New Note in MacVim.sh
+++ Mac/Scripts/New Note in MacVim.sh
@@ -1,3 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -eu
+shopt -s extglob
+
 PATH+=:/opt/homebrew/bin
-mvim ~/Notes/$(date +%m%d.md) +"cd ~/Notes"
+title=$(basename "$0" .sh)
+iconPath=/Applications/MacVim.app/Contents/Resources/MacVim.icns
+
+if name=$(
+    osascript -e "get text returned of (\
+        display dialog \"Create a new note named:\" \
+            with title \"$title\" \
+            $(! [ -e "$iconPath" ] || echo "with icon POSIX file \"$iconPath\"") \
+            default answer \"$(date +%m%d) \" \
+        )"
+); then
+    name=${name##+([[:space:]])}
+    name=${name%%+([[:space:]])}
+    exec mvim ~/Notes/"$name".md +"cd %:h"
+fi
