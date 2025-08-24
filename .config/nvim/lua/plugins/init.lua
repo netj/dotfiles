e72d916cdaa2a2e3dd1fe2dccb729631b844559a
Jaeho Shin <netj@sparcs.org>
2025-08-24 00:14:09 -0700

    nvim config bootstrapped with NvChad
        git clone https://github.com/NvChad/starter ~/.config/nvim && nvim

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
